afd2b33e0285bb745b8e3de5dbbbc8839642c4e5
anonygrits <anonygrits@gmail.com>
2018-05-11 05:36:09 -0500

    fix movie list url

--- app/src/main/java/com/example/android/popularmovies1/Utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/popularmovies1/Utilities/NetworkUtils.java
@@ -10,11 +10,26 @@ import java.net.URL;
 import java.util.Scanner;
 
 public final class NetworkUtils {
-    private final String POPULAR_MOVIES_URL="https://developers.themoviedb.org/3/movies/get-popular-movies";
-    private final String TOP_MOVIES_URL="https://developers.themoviedb.org/3/movies/get-top-rated-movies";
+    private final String POPULAR_MOVIES_URL="https://api.themoviedb.org/3/movie/popular?";
+    private final String TOP_MOVIES_URL="https://api.themoviedb.org/3/movie/top_rated?";
+
+    private static String API_KEY="api_key";
+
 
     // get movie list from API (adapted from Sunshine S02.01 Solution)
-    public static String getMovieList(URL url) throws IOException {
+    public static String getMovieList(String movie_url, String api_key, String api) throws IOException {
+        // build url inside this function but think about how to make a generic one to combine with thumbnail url builder
+        Uri thumbnailURI = Uri.parse(movie_url).buildUpon()
+                .appendQueryParameter(api_key,api)
+                .build();
+        URL url = null;
+        try {
+            url = new URL(thumbnailURI.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        // make http connection and get data
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         try {
             InputStream in = urlConnection.getInputStream();
@@ -51,7 +66,4 @@ public final class NetworkUtils {
         }
         return url;
     }
-
-
-
 }
