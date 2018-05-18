0aa929e1d4e3bb0896470afc39336b5ec27f05fa
anonygrits <anonygrits@gmail.com>
2018-05-18 03:21:25 -0500

    clean up Movie pojo code

--- app/src/main/java/com/example/android/popularmovies1/Movie.java
+++ app/src/main/java/com/example/android/popularmovies1/Movie.java
@@ -7,44 +7,58 @@ public class Movie {
     private String mId;
     private String mPoster_path;
     private String mTitle;
-    private String mRelease_date;
     private String mRelease_year;
     private String mVote_average;
     private String mOverview;
 
-    public Movie(String id, String poster_path, String title, String release_date, String vote_average, String overview) {
+    public void setId(String id) {
         mId = id;
-        mPoster_path = poster_path;
-        mTitle = title;
-        mRelease_date = release_date;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
-        LocalDate localDate = LocalDate.parse(mRelease_date, formatter);
-        mRelease_year = Integer.toString(localDate.getYear());
-        mVote_average = vote_average;
-        mOverview = overview;
     }
 
     public String getId() {
         return mId;
     }
 
+    public void setPoster_path(String poster_path) {
+        mPoster_path = poster_path;
+    }
+
     public String getPoster_path() {
         return mPoster_path;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
     }
 
     public String getTitle() {
         return mTitle;
     }
 
+    public Void setRelease_year(String release_date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+        LocalDate localDate = LocalDate.parse(release_date, formatter);
+        mRelease_year = Integer.toString(localDate.getYear());
+    }
+
     public String getRelease_year() {
         return mRelease_year;
+    }
+
+    public Void setVote_average(String vote_average) {
+        mVote_average = vote_average;
     }
 
     public String getVote_average() {
         return mVote_average;
     }
 
+    public Void setOverview(String overview) {
+        mOverview = overview;
+    }
+
     public String getOverview() {
         return mOverview;
     }
+
 }
